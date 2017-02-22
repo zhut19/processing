72f58c7469344db129d290da8b37fb02f2e4fac3
Qing Lin <mcfatelin@gmail.com>
2017-02-22 14:32:17 -0600

    Added the batch mode for the area correlated S1&S2 mode

---
+++ montecarlo/fax_waveform/MidwayBatch_AreaCorrelatedS1S2.py
@@ -0,0 +1,101 @@
+####################################
+## batch code for WF simulation
+####################################
+import sys, array, os, getpass
+from subprocess import call
+import subprocess as subp
+import time
+import math as math
+from subprocess import Popen, PIPE
+
+if len(sys.argv)<2:
+    print("========= Syntax ========")
+    print("python MidwayBatch_AreaCorrelatedS1S2.py ....")
+    print("<Output path (abs.)>")
+    print("<number of jobs>")
+    print("<number of events in each job>")
+    print("<enable PMT after pulses ?(0 for disable)>")
+    print("<enable S2 after pulses ?(0 for disable)>")
+    print("<2D band for S1-S2 area correlation (abs.)>")
+    print("<g1 value>")
+    print("<g2 value>")
+    print("<If use Public node (0 for no(xenon1t nodes); 1 for yes; 2 for kicp nodes)>")
+    exit()
+
+OutputGeneralPath = sys.argv[1]
+NumJobs = int(sys.argv[2])
+NumEvents = int(sys.argv[3])
+PMTAfterpulseFlag = int(sys.argv[4])
+S2AfterpulseFlag = int(sys.argv[5])
+Input2DBandFile = sys.argv[6]
+Nomial_g1 = float(sys.argv[7])
+Nomial_g2 = float(sys.argv[8])
+IfUsePublicNodes = int(sys.argv[9])
+
+MaxNumJob = 64
+if not IfUsePublicNodes:
+    MaxNumJob=200
+
+
+##### Start batching #########
+CurrentPath = os.getcwd()
+print (CurrentPath)
+CurrentUser = getpass.getuser()
+for i in range(NumJobs):
+
+    RunString = "%06d" % i
+    
+    # create folder
+    OutputPath = OutputGeneralPath + "/" + RunString
+    if os.path.exists(OutputPath):
+        subp.call("rm -r "+OutputPath, shell=True)
+    subp.call("mkdir -p "+OutputPath, shell=True)
+    
+    # define filenames
+    SubmitFile = OutputPath+"/submit_"+ RunString + ".sh"
+    SubmitOutputFilename = OutputPath+"/submit_"+ RunString + ".log"
+    SubmitErrorFilename = OutputPath+"/submit_"+ RunString + ".log"
+
+    # create the basic submit 
+    subp.call("echo '#!/bin/bash\n' >> "+SubmitFile, shell=True)
+    subp.call("echo '#SBATCH --output="+SubmitOutputFilename+"' >> "+SubmitFile, shell=True)
+    subp.call("echo '#SBATCH --error="+SubmitErrorFilename+"' >> "+SubmitFile, shell=True)
+    subp.call("echo '#SBATCH --time=03:59:00' >> "+SubmitFile, shell=True)
+    subp.call("echo '#SBATCH --account=pi-lgrandi' >> "+SubmitFile, shell=True)
+    if IfUsePublicNodes==0:
+        subp.call("echo '#SBATCH --qos=xenon1t' >> "+SubmitFile, shell=True)
+        subp.call("echo '#SBATCH --partition=xenon1t\n' >> "+SubmitFile, shell=True)
+    elif IfUsePublicNodes==2:
+        subp.call("echo '#SBATCH --qos=xenon1t-kicp' >> "+SubmitFile, shell=True)
+        subp.call("echo '#SBATCH --partition=kicp\n' >> "+SubmitFile, shell=True)
+
+    Command = CurrentPath+"/./run_fax_AreaCorrelatedS1S2.sh "+Input2DBandFile+""+str(Nomial_g1)+" "+str(Nomial_g2)+" "+str(PMTAfterpulseFlag)+" "+str(S2AfterpulseFlag)+" "+str(NumEvents)+" "+OutputGeneralPath+" "+RunString)
+    subp.call("echo '"+Command+"\n' >> "+SubmitFile, shell=True)
+
+    SubmitPath = OutputPath
+
+    #submit
+    IfSubmitted=0
+    while IfSubmitted==0:
+        Partition = "sandyb" # public
+        if not IfUsePublicNodes:
+            Partition = "xenon1t"
+        elif IfUsePublicNodes==2:
+            Partition = "kicp"
+        p1 = Popen(["squeue","--partition="+Partition, "--user="+CurrentUser], stdout=PIPE)
+        p2 = Popen(["wc", "-l"], stdin=p1.stdout, stdout=PIPE)
+        p1.stdout.close()  # Allow p1 to receive a SIGPIPE if p2 exits.
+        output = p2.communicate()[0]
+        Status=subp.call("squeue --partition="+Partition+" --user="+CurrentUser +" | wc -l", shell=True)
+        Output=int(output)
+        #print(Status)
+        
+        print("Current job running number "+str(Output))            
+
+        if Status==0 and Output<MaxNumJob:
+            #sbatch it 
+            subp.call("cd "+SubmitPath+";sbatch "+SubmitFile+";cd -", shell=True)
+            IfSubmitted=1
+            time.sleep(2.0)
+        else:
+            time.sleep(30) 
---
+++ montecarlo/fax_waveform/run_fax_AreaCorrelatedS1S2.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env bash
+############################################
+#
+# Usage: Need to modify all parameters and paths below, then:
+#           ./run_fax.sh <output directory> <subrun number>
+#
+############################################
+
+echo "Start time: " `/bin/date`
+echo "Job is running on node: " `/bin/hostname`
+echo "Job running as user: " `/usr/bin/id`
+echo "Job is running in directory: $PWD"
+
+###### General parameters #####
+Detector=XENON1T
+
+###### Simulation parameters #####
+Input2DBandFile=$1
+Nomial_g1=$2
+Nomial_g2=$3
+
+RecoilType=ER
+
+echo 'IfS1S2Correlation = ${IfS1S2Correlation}'
+# enable s2 after pulse depending on the argument
+# 1 for enable
+PMTAfterpulseEnableFlag=$4
+S2AfterpulseEnableFlag=$5
+
+# Select fax+pax version
+#~ PAXVERSION=head
+PAXVERSION=v6.5.0 # temporary change
+
+# Specify number of events
+NumEvents=$6
+
+# This run number (from command line argument)
+SUBRUN=$8
+
+########################################
+
+# Setup the software
+CVMFSDIR=/cvmfs/xenon.opensciencegrid.org
+#export PATH="${CVMFSDIR}/releases/anaconda/2.4/bin:$PATH"
+export PATH="/project/lgrandi/anaconda3/bin:$PATH"
+source activate pax_${PAXVERSION} &> /dev/null
+
+# Use path of this script for Python scripts below
+# (In case user modified them)
+#MY_PATH=`dirname \"$0\"`
+MY_PATH=$(cd `dirname $0`; pwd)
+echo $MY_PATH
+RELEASEDIR=`( cd "$MY_PATH" && pwd )`
+
+# Setting up directories
+#start_dir=$PWD
+
+
+OUTDIR=$7/${SUBRUN}
+mkdir -p ${OUTDIR}
+cd ${OUTDIR}
+
+#if [ "$OSG_WN_TMP" == "" ];
+#then
+#    OSG_WN_TMP=$PWD
+#fi
+#cd $OSG_WN_TMP
+#
+#work_dir=`mktemp -d --tmpdir=$OSG_WN_TMP`
+#cd $work_dir
+
+# Filenaming
+FILEROOT=FakeWaveform_${Detector}_${SUBRUN}
+FILENAME=${OUTDIR}/${FILEROOT}
+CSV_FILENAME=${FILENAME}.csv       # Fake input data
+FAX_FILENAME=${FILENAME}_truth # fax truth info
+PKL_FILENAME=${FILENAME}_truth.pkl # converted fax truth info
+RAW_FILENAME=${FILENAME}_raw       # fax simulated raw data
+PAX_FILENAME=${FILENAME}_pax       # pax processed data
+PAX_FILENAME_WOPATH=${FILEROOT}_pax       # pax processed data without path
+HAX_FILENAME=${FILENAME}_hax       # hax reduced data
+CustomIniFilename=${RELEASEDIR}/NoS2Afterpulses.ini
+NoPMTAfterpulseIniFilename=${RELEASEDIR}/NoPMTAfterpulses.ini
+echo ${CustomIniFilename}
+
+
+# Create the fake input data
+python ${RELEASEDIR}/CreateFakeCSV_CorrelatedS1S2.py ${Detector} ${Input2DBandFile} ${Nomial_g1} ${Nomial_g2} ${NumEvents} ${RecoilType} ${CSV_FILENAME}
+
+# Start of simulations #
+
+# fax stage
+if (($S2AfterpulseEnableFlag==0)); then
+	if (($PMTAfterpulseEnableFlag==0)); then
+		echo 'Both S2 and PMT afterpulse disabled'
+		(time paxer --input ${CSV_FILENAME} --config ${Detector} reduce_raw_data Simulation --config_path ${NoPMTAfterpulseIniFilename} ${CustomIniFilename} --config_string "[WaveformSimulator]truth_file_name=\"${FAX_FILENAME}\"" --output ${RAW_FILENAME};) &> ${RAW_FILENAME}.log
+	else
+		echo 'Only S2 afterpulse disabled'
+		(time paxer --input ${CSV_FILENAME} --config ${Detector} reduce_raw_data Simulation --config_path ${CustomIniFilename} --config_string "[WaveformSimulator]truth_file_name=\"${FAX_FILENAME}\"" --output ${RAW_FILENAME};) &> ${RAW_FILENAME}.log
+	fi
+else
+	if (($PMTAfterpulseEnableFlag==0)); then
+		echo 'Only PMT afterpulse disabled'
+		(time paxer --input ${CSV_FILENAME} --config ${Detector} reduce_raw_data Simulation --config_path ${NoPMTAfterpulseIniFilename} --config_string "[WaveformSimulator]truth_file_name=\"${FAX_FILENAME}\"" --output ${RAW_FILENAME};) &> ${RAW_FILENAME}.log
+	else
+		echo 'Both S2 and PMT afterpulse enabled'
+		(time paxer --input ${CSV_FILENAME} --config ${Detector} reduce_raw_data Simulation  --config_string "[WaveformSimulator]truth_file_name=\"${FAX_FILENAME}\"" --output ${RAW_FILENAME};) &> ${RAW_FILENAME}.log
+	fi
+fi
+
+#	(time paxer --input ${CSV_FILENAME} --config ${Detector} reduce_raw_data Simulation --config_string "[WaveformSimulator]truth_file_name=\"${FAX_FILENAME}\"" --output ${RAW_FILENAME};) &> ${RAW_FILENAME}.log
+
+
+# convert fax truth to pickle
+python ${RELEASEDIR}/ConvertFaxTruthToPickle.py ${FAX_FILENAME} ${PKL_FILENAME}
+
+# pax stage
+(time paxer --ignore_rundb --input ${RAW_FILENAME} --config ${Detector} --output ${PAX_FILENAME};) &> ${PAX_FILENAME}.log
+
+# hax stage
+HAXPYTHON="import hax; "
+HAXPYTHON+="hax.init(main_data_paths=['${OUTDIR}'], minitree_paths=['${OUTDIR}'], pax_version_policy = 'loose'); "
+HAXPYTHON+="hax.minitrees.load('${PAX_FILENAME##*/}', ['Basics', 'Fundamentals']);"
+
+(time python -c "${HAXPYTHON}";)  &> ${HAX_FILENAME}.log
+
+
+# custom minitree
+(time python ${RELEASEDIR}/ReduceDataNormal.py ${PAX_FILENAME_WOPATH} ${OUTDIR};) &> ${HAX_FILENAME}.log
+
+
+# Cleanup
+rm -f pax*
+
+
+#cd $start_dir
+#rm -fr $work_dir
